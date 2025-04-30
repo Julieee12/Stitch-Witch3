8cdab5c9d54506c67e97f026c0fe5f2e0c10e7dd
Julieee12 <anaiuliaopris96@gmail.com>
2025-04-30 11:16:32 +0200

    styled the counter page

--- stitch_witch_aid/lib/root/brand-colors.dart
+++ stitch_witch_aid/lib/root/brand-colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class BrandColors {
   static const Color purpleLight = Color(0xFFb079c2);
   static const Color purpleLighter = Color(0xFFd3aada);
+  static const Color purpleLightish = Color(0xFFc79bce);
   static const Color purpleVeryLight = Color(0xFFf0eaf0);
   static const Color purpleMedium = Color(0xFFb295eb);
   static const Color purpleDark = Color(0xFF8c52ff);
