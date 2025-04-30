92cf2e9306434d99a38d33507538ed70379ef56e
Julieee12 <anaiuliaopris96@gmail.com>
2025-04-30 09:44:06 +0200

    idk I hope this doesnt break main

--- stitch_witch_aid/lib/inventory/inventory.dart
+++ stitch_witch_aid/lib/inventory/inventory.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:stitch_witch_aid/root/tags.dart';
 import '../root/brand-colors.dart';
-import '../root/search-bar.dart';
 
 class Inventory extends StatelessWidget {
   const Inventory({super.key});
@@ -10,25 +8,18 @@ class Inventory extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: BrandColors.purpleExtraLight,
-        child: Stack(
-          children: [
-            Tags(["in","ven", "tory"]),
-            TopSearchBar(caller: this),
-            Padding(
-              padding: const EdgeInsets.only(top: 110),
-              child: GridView.count(
-                //shrinkWrap: true,
-                padding: const EdgeInsets.all(20),
-                crossAxisCount: 2,
-                crossAxisSpacing: 30,
-                mainAxisSpacing: 15,
-                children: List.generate(10, (index) {
-                  return _buildGridItem(BrandColors.purpleSoft);
-                }),
-              ),
-            ),
-          ],
+      child: Padding(
+        padding: const EdgeInsets.only(top: 110),
+        child: GridView.count(
+          padding: const EdgeInsets.all(20),
+          crossAxisCount: 2,
+          crossAxisSpacing: 30,
+          mainAxisSpacing: 15,
+          children: List.generate(10, (index) {
+            return _buildGridItem(BrandColors.purpleSoft);
+          }),
         ),
+      ),
     );
   }
 
