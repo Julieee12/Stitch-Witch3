283123cccb3155a1cd39f57bf3faf5d0ccac2207
Julieee12 <anaiuliaopris96@gmail.com>
2025-05-25 17:36:52 +0200

    updated intervals

--- stitch_witch_aid/lib/counter/stitch-detector.dart
+++ stitch_witch_aid/lib/counter/stitch-detector.dart
@@ -1,14 +1,14 @@
 class StitchDetector {
   // Fictional intervals - replace these with your actual analysis results
   static const int X_MIN = 7000;
-  static const int X_MAX = 13500;
-  static const int Y_MIN = -4000;
-  static const int Y_MAX = 24000;
-  static const int Z_MIN = -10000;
-  static const int Z_MAX = 11000;
+  static const int X_MAX = 11500;
+  static const int Y_MIN = 7000;
+  static const int Y_MAX = 15000;
+  static const int Z_MIN = 3000;
+  static const int Z_MAX = 9500;
 
   // Prevent double counting
-  static const int MIN_SAMPLES_BETWEEN_STITCHES = 3;
+  static const int MIN_SAMPLES_BETWEEN_STITCHES = 10;
   int _samplesSinceLastStitch = 0;
 
   bool processAccelerometerData(int ax, int ay, int az) {
